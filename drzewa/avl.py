69c911f1c930f4c2f3068bcc63cf1437a9c68191
domikado5 <dominik.sawicki52@gmail.com>
2020-04-15 21:21:36 +0200

    Added rotation to AVL construct

--- drzewa/avl.py
+++ drzewa/avl.py
@@ -12,7 +12,10 @@ class Tree:
 
     def insert(self, leaf, parrent):
         if parrent is None:
-            self.increaseHeight(leaf.parrent)
+            if leaf.parrent is None:
+                self.root = leaf
+            else:
+                self.increaseHeight(leaf.parrent)
             return leaf
         elif parrent.value > leaf.value:
             leaf.parrent = parrent
@@ -30,28 +33,84 @@ class Tree:
         balanceFactor = self.getBalanceFactor(parrent)
 
         if balanceFactor > 1: #left branch is higher than right branch
-            if leaf.value < parrent.leaf.value: # LL rotation
-                pass
-            elif leaf.value > parrent.leaf.value: # LR rotation
-                pass
-        elif balanceFactor < -1 and leaf.value > parrent.right.value: #right branch is higher than left branch
-            if leaf.value > parrent.right.value: # RR rotation
-                pass
+            if leaf.value < parrent.left.value: # LL rotation
+                return self.rotateRight(parrent)
+            elif leaf.value >= parrent.left.value: # LR rotation
+                parrent.left = self.rotateLeft(parrent.left)
+                return self.rotateRight(parrent)
+        elif balanceFactor < -1: #right branch is higher than left branch
+            if leaf.value >= parrent.right.value: # RR rotation
+                return self.rotateLeft(parrent)
             elif leaf.value < parrent.right.value: # RL rotation
-                pass
+                print("ROOT")
+                print(self.root.value)
+                print("PARRENT RIGHT")
+                print(parrent.right.value)
+                parrent.right = self.rotateRight(parrent.right)
+                print("ROOT")
+                print(self.root.value)
+                print("PARRENT")
+                print(parrent.value)
+                return self.rotateLeft(parrent)
     
     def increaseHeight(self, parrent, i=1):
         i += 1
+        if parrent is None:
+            return None
         if parrent.height < i: # checking current height
             parrent.height = i
         if parrent.parrent is not None:
             self.increaseHeight(parrent.parrent, i)
 
-    def leftRotate():
-        pass
+    def rotateLeft(self, parrent):
+        y = parrent.right
+        z = y.left
+        y.left = parrent
+        y.parrent = parrent.parrent
+        parrent.parrent = y
+        parrent.right = z
+        if z is not None:
+            z.parrent = parrent
 
-    def rightRotate():
-        pass
+        parrent.height = self.updateHeight(parrent)
+        y.height = self.updateHeight(y)
+        if y.parrent is None: # Updating root
+            self.root = y
+        elif self.isRoot(y.parrent): # Updating root childs
+            if y.parrent.left == parrent:
+                self.root.left = y
+            elif y.parrent.right == parrent:
+                self.root.right = y
+            self.root.height = self.updateHeight(self.root)
+        return y
+    
+    def rotateRight(self, parrent):
+        y = parrent.left
+        z = y.right
+
+        y.right = parrent
+        y.parrent = parrent.parrent
+        parrent.parrent = y
+        parrent.left = z
+        if z is not None:
+            z.parrent = parrent
+
+        parrent.height = self.updateHeight(parrent)
+        y.height = self.updateHeight(y)
+        if y.parrent is None: # Updating root
+            self.root = y
+        elif self.isRoot(y.parrent): # Updating root childs
+            if y.parrent.left == parrent:
+                self.root.left = y
+            elif y.parrent.right == parrent:
+                self.root.right = y
+            self.root.height = self.updateHeight(self.root)
+        return y
+
+    def updateHeight(self, parrent):
+        a = 0 if parrent.left is None else parrent.left.height
+        b = 0 if parrent.right is None else parrent.right.height
+        return 1+max(a,b)
 
     def getBalanceFactor(self, parrent):
         if parrent.left is None:
@@ -93,14 +152,23 @@ class Tree:
             route.append(root.value)
             self.min(root.left)
 
+    def isRoot(self, parrent):
+        if parrent.parrent is None:
+            return True
+        else:
+            return False
+
 myTree = Tree()
-myTree.root = Leaf(10) # define root of tree
-myTree.insert(Leaf(11), myTree.root)
-myTree.insert(Leaf(11), myTree.root)
-myTree.insert(Leaf(11), myTree.root)
-myTree.insert(Leaf(9), myTree.root)
-myTree.printTree(myTree.root)
+myTree.insert(Leaf(10), myTree.root)
+myTree.insert(Leaf(20), myTree.root)
+myTree.insert(Leaf(30), myTree.root)
+myTree.insert(Leaf(40), myTree.root)
+myTree.insert(Leaf(50), myTree.root)
+myTree.insert(Leaf(25), myTree.root)
+print(myTree.root.value)
+#myTree.printTree(myTree.root)
 print("Wysokosc drzewa:")
 print(myTree.root.height)
 myTree.max(myTree.root)
-myTree.min(myTree.root)
+#myTree.min(myTree.root)\
+print(myTree.root.left.left.value)
