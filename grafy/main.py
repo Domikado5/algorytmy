8dfe6f3f24857ba27c37cf79a5b5c9856d7346f3
domikado5 <dominik.sawicki52@gmail.com>
2020-06-04 22:28:50 +0200

    Added BFS algorithm

--- grafy/main.py
+++ grafy/main.py
@@ -2,28 +2,30 @@ import numpy as np
 import random as rn
 
 
-vertices = 5 # liczba wierzcholkow
+vertices = 10 # liczba wierzcholkow
 neighborhoodMatrix = np.zeros((vertices,vertices), dtype=int) # macierz sasiedztw / neighborhoodMatrix
 neighborhoodList = [[] for i in range(vertices)]
 edgesTable = []
 
-def generateGraph(nM, nL, eT, v): # generowanie grafu prosta metodą / wypełnianie tylko wartości w górnym trójkącie
-    # every column need one or more connection
+def generateDirectedGraph(nM, nL, eT, v): # generowanie grafu prosta metodą / wypełnianie tylko wartości w górnym trójkącie
+    # every column need one or more connections
     edges = (vertices*(vertices-1))//4
     bonusEdges = edges - (v - 1)
-    players = [i for i in range(2, v)]
+    players = [ i for i in range(2, v) ]
     winners = np.zeros(bonusEdges, dtype=int)
     i = 0
     begin = 2
+
     while bonusEdges > 0:
         winner = rn.choice(players)
         count = np.count_nonzero(winners == winner)
-        if count < winner:
+        if count < winner-1:
             winners[i] = winner
             bonusEdges -= 1
             i += 1
         else:
             players.remove(winner)
+
     for i in range(1, v):
         nM[rn.randint(0,i-1)][i] = 1
         count = np.count_nonzero(winners == i)
@@ -33,13 +35,39 @@ def generateGraph(nM, nL, eT, v): # generowanie grafu prosta metodą / wypełnia
             j = np.random.choice(findBonus)
             nM[j][i] = 1
             count -= 1
+
     for i in range(v):
         connections = np.where(nM[i][:] == 1)
         nL[i] = list(connections[0])
         eT += [[i,j] for j in nL[i]] 
     
+def generateNonDirectedGraph():
+    pass
 
-generateGraph(neighborhoodMatrix, neighborhoodList, edgesTable, vertices)
+
+def BFS(nL, v):
+    visited = []
+    queue = []
+    visited.append(0)
+    queue = np.unique(np.concatenate((queue, nL[0])))
+    queue = queue.astype('int32')
+    print("Odwiedzone", visited, " Kolejka ",queue)
+    while len(visited) < v:
+        nextVer = queue[0]
+        visited.append(nextVer)
+        for i in range(len(nL[nextVer])):
+            if nL[nextVer][i] not in visited and nL[nextVer][i] not in queue: # sprawdzanie czy wierzchołek już wystąpił
+                queue = np.append(queue, nL[nextVer][i])
+        queue = np.delete(queue, 0)
+        print("Odwiedzone", visited, " Kolejka ", queue)
+        
+
+def DFS():
+    pass
+
+generateDirectedGraph(neighborhoodMatrix, neighborhoodList, edgesTable, vertices)
 print("Macierz sasiedztw: \n", neighborhoodMatrix)
 print("Lista następnikow: \n", neighborhoodList)
 print("Tabela krawedzi: \n", edgesTable)
+print("Przechodzenie wszerz:")
+BFS(neighborhoodList, vertices)
